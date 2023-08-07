8b889634feca6804463a584dd477959766d01229
afreuljoy <afreuljoy@gmail.com>
2023-08-07 12:51:18 +0600

    card design update

--- client/src/Components/Card/ProductCard.jsx
+++ client/src/Components/Card/ProductCard.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { FaStar } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   console.log(product);
@@ -15,48 +18,82 @@ const ProductCard = ({ product }) => {
     badgeText,
   } = product;
 
+  // Function to generate stars based on the rating
+  const renderRatingStars = (rating) => {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+
+    const starsArray = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      starsArray.push(<FaStar key={i} />);
+    }
+
+    if (hasHalfStar) {
+      starsArray.push(<FaStar key={fullStars} half />);
+    }
+
+    return starsArray;
+  };
+
   return (
-    <div className="bg-white shadow-md overflow-hidden relative w-60 h-96">
-      {/* Badge */}
-      <div
-        className="bg-red-500 text-white px-2 py-1 top-2 left-0 t text-xs  font-light absolute"
-        style={{ backgroundColor: "#F44336" }}
-      >
-        {badgeText}
-      </div>
+    <Link to={`/product/${id}`}>
+      <div className="bg-white shadow-md overflow-hidden relative w-60 h-96 transition-transform hover:scale-105">
+        {/* Heart Icon */}
+        <div className="absolute top-4 right-4">
+          <Link to={`/product/${id}`}>
+            <AiOutlineHeart
+            size={30}
+            className="text-gray-400 hover:text-rose-500 transition-colors "
+          />
+          </Link>
+        </div>
 
-      {/* Image */}
-      <div className="p-4">
-        <img
-          src={imgSrc}
-          alt={bookName}
-          className="w-full h-40 object-contain mb-4"
-        />
+        {/* Badge */}
+        <div
+          className="bg-red-500 text-white px-2 py-1 top-2 left-0 t text-xs font-light absolute"
+          style={{ backgroundColor: "#F44336" }}
+        >
+          {badgeText}
+        </div>
 
-        {/* Content */}
-        <div>
-          <h3 className="text-base text-center ">{bookName}</h3>
-          <p className="text-gray-600 italic text-sm text-center ">- By {author}</p>
+        {/* Image */}
+        <div className="p-4">
+          <img
+            src={imgSrc}
+            alt={bookName}
+            className="w-full h-40 object-contain mb-4"
+          />
 
-          {/* Price */}
-          <div className="flex justify-between items-center mt-4 ">
-            <p>
-              <b>Rs. {discountedPrice} &nbsp;&nbsp;</b>
-              <del>Rs. {originalPrice}</del> &nbsp;&nbsp;
-              <span className="text-rose-500 text-xs" >({discountPercent}% off)</span>
+          {/* Content */}
+          <div>
+            <h3 className="text-base text-center">{bookName}</h3>
+            <p className="text-gray-600 italic text-sm text-center">
+              - By {author}
             </p>
-          </div>
 
-          {/* Rating and Add to Cart */}
-          <div className="flex justify-between items-center mt-2">
-            <p className="text-gray-600">Rating: {rating}</p>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded transition-colors">
-              Add to Cart
-            </button>
+            {/* Price */}
+            <div className="flex items-center justify-center mt-4">
+              <p>
+                <b>TK {discountedPrice} &nbsp;&nbsp;</b>
+                <del>TK {originalPrice}</del> &nbsp;&nbsp;
+                <span className="text-rose-500 text-xs">
+                  ({discountPercent}% off)
+                </span>
+              </p>
+            </div>
+
+            {/* Rating */}
+            <div className="flex justify-center items-center mt-2">
+              <p className="text-gray-600 flex">
+                {renderRatingStars(rating)}
+                <span className="ml-1 text-sm">({rating.toFixed(1)})</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- client/src/Pages/Shop/Shop.jsx
+++ client/src/Pages/Shop/Shop.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import ProductCard from '../../Components/Card/ProductCard';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../../Components/Card/ProductCard";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    console.log(products);
-    // Fetch products data from the JSON file using Axios
-    useEffect(() => {
-      axios
-        .get('../../.././public//bookData.json') 
-        .then((response) => {
-          setProducts(response.data);
-        })
-        .catch((error) => {
-          console.error('Error fetching products:', error);
-        });
-    }, []);
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  // Fetch products data from the JSON file using Axios
+  useEffect(() => {
+    axios
+      .get("../../.././public//bookData.json")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
 
   return (
     <div className="flex flex-wrap">
@@ -36,12 +36,15 @@ const Shop = () => {
 
       {/* Left Column */}
       <div className="w-full md:w-3/4 p-4">
-        <h2 className="text-xl font-bold mb-4">Products</h2>
+        <h2 className="text-xl font-bold mb-4 text-center underline   ">
+          {" "}
+          Number Of Products : {products.length}
+        </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
