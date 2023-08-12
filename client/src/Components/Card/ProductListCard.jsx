c93eb829c4dec7f330509ce5b5e685c422569b1b
afreuljoy <afreuljoy@gmail.com>
2023-08-12 13:36:30 +0600

    grid and list updated

--- client/src/Components/Card/ProductListCard.jsx
+++ client/src/Components/Card/ProductListCard.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { FaStar } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
@@ -17,7 +16,6 @@ const ProductListCard = ({ product }) => {
     badgeText,
   } = product;
 
-  // Function to generate stars based on the rating
   const renderRatingStars = (rating) => {
     const fullStars = Math.floor(rating);
     const hasHalfStar = rating % 1 !== 0;
@@ -36,43 +34,40 @@ const ProductListCard = ({ product }) => {
   };
 
   return (
-    <Link to={`/product/${id}`} className="flex items-start space-x-4 p-4 border-b border-gray-200">
-      {/* Image */}
-      <img src={imgSrc} alt={bookName} className="w-24 h-32 object-contain" />
+    <Link
+      to={`/product/${id}`}
+      className="flex items-start space-x-4 p-4 border-b border-gray-200 hover:bg-gray-100 transition duration-300"
+    >
+      <img src={imgSrc} alt={bookName} className="w-24 h-32 object-cover rounded" />
 
-      {/* Content */}
       <div className="flex-grow">
-        <h3 className="text-base">{bookName}</h3>
-        <p className="text-gray-600 italic text-sm">- By {author}</p>
+        <h3 className="text-lg font-semibold">{bookName}</h3>
+        <p className="text-gray-600 text-sm italic">By {author}</p>
 
-        {/* Price */}
         <div className="mt-2">
-          <p>
-            <b>TK {discountedPrice}</b>
+          <p className="text-xl font-medium">
+            TK {discountedPrice}
             <span className="text-rose-500 text-xs ml-2">
               ({discountPercent}% off)
             </span>
           </p>
-          <p>
+          <p className="text-gray-500">
             <del>TK {originalPrice}</del>
           </p>
         </div>
 
-        {/* Rating */}
         <div className="flex items-center mt-2">
           {renderRatingStars(rating)}
-          <span className="ml-1 text-sm">({rating.toFixed(1)})</span>
+          <span className="ml-1 text-sm">{rating.toFixed(1)}</span>
         </div>
       </div>
 
-      {/* Heart Icon */}
       <Link to={`/product/${id}`} className="text-gray-400 hover:text-rose-500">
         <AiOutlineHeart size={30} />
       </Link>
 
-      {/* Badge */}
       <div
-        className="bg-red-500 text-white px-2 py-1 text-xs font-light"
+        className="bg-red-500 text-white px-2 py-1 text-xs font-semibold rounded"
         style={{ backgroundColor: "#F44336" }}
       >
         {badgeText}
