ca8ffcc7f6333219a6843fc65dffb0245f795cff
afreuljoy <afreuljoy@gmail.com>
2023-08-13 12:00:23 +0600

    navbar modify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,19 @@ import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Shop from "./Pages/Shop/Shop";
 import ShopContextProvider from "./context/ShopContext";
+import SubNavbar from "./Components/SubNavbar/SubNavbar";
+import ComingSoon from "./Components/ComingSoon/ComingSoon";
 
 function App() {
   return (
     <>
       <ShopContextProvider>
+        <SubNavbar />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/coming" element={<ComingSoon/>} />
         </Routes>
         <Footer />
       </ShopContextProvider>
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import brandLogo from "../../assets/Images/EduNation.png";
+import Select from "react-select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const options = [
+  { value: "contact", label: "Contact Us" },
+  { value: "faq", label: "FAQ" },
+  { value: "terms", label: "Terms & Conditions" },
+  { value: "privacy", label: "Privacy Policy" },
+];
+
+const defaultOption = { value: "help", label: "Help" };
 
 const Navbar = () => {
   return (
     <nav className="bg-black text-white p-4">
       <div className="container mx-auto">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-center items-center space-x-4">
           <div>
-            <Link to="/" className="text-2xl font-bold">
-              EDUNATION Bookstore
+            <Link to="/">
+              <img
+                src={brandLogo}
+                alt="Brand Logo"
+                className="h-10 w-32 md:h-10 md:w-36"
+              />
             </Link>
           </div>
-          <div>
-            <ul className="flex space-x-4">
-              <li>
-                <Link to="/" className="hover:text-gray-300">Home</Link>
-              </li>
-              <li>
-                <Link to="/shop" className="hover:text-gray-300">Browse Books</Link>
-              </li>
-              <li>
-                <Link to="/about" className="hover:text-gray-300">About Us</Link>
-              </li>
-              <li>
-                <Link to="/contact" className="hover:text-gray-300">Contact Us</Link>
-              </li>
-            </ul>
+          {/* Search Bar */}
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-2 px-4 pr-10 rounded-full bg-gray-800 text-white focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <button className="absolute right-0 top-0 bottom-0 px-3 py-2">
+              <span className="text-gray-300">Search</span>
+            </button>
+          </div>
+
+          <div className=" text-white">
+            <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+          </div>
+
+          {/* Select */}
+          <div className="w-32">
+            <Select
+              options={options}
+              defaultValue={defaultOption}
+              className="text-black"
+            />
           </div>
         </div>
       </div>
